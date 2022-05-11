3283901e3556b286e11d35847097206929797abd
Parker74 <1227298453@qq.com>
2022-05-11 14:29:38 +0800

    first test++

---
+++ reggie_take_out/src/main/java/com/itheima/Controller/employeeController.java
@@ -0,0 +1,9 @@
+package com.itheima.Controller;
+
+/**
+ * @author HaoXiaoLong
+ * @version 1.0
+ * @date 2022/5/11 14:19
+ */
+public class employeeController {
+}
---
+++ reggie_take_out/src/main/java/com/itheima/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.itheima.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.entity.Employee;
+
+/**
+ * @author HaoXiaoLong
+ * @version 1.0
+ * @date 2022/5/11 14:21
+ */
+public interface EmployeeService extends IService<Employee> {
+}
