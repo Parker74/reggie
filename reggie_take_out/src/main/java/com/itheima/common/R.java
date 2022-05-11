7399f30d1f5a9820268534a89b110108fb879118
Parker74 <1227298453@qq.com>
2022-05-11 14:36:04 +0800

    Commit

--- reggie_take_out/src/main/java/com/itheima/common/R.java
+++ reggie_take_out/src/main/java/com/itheima/common/R.java
@@ -11,7 +11,7 @@ import java.util.Map;
  * @date 2022/5/11 14:31
  */
 @Data
-public class R {
+public class R<T> {
     private Integer code; //编码：1成功，0和其它数字为失败
     private String msg; //错误信息
     private T data; //数据
