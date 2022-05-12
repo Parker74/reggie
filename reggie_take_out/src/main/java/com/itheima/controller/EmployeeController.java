0ded554ee336c18a4a4d62848f3da28fd035d60f
Parker74 <1227298453@qq.com>
2022-05-12 16:51:28 +0800

    新增员工

--- reggie_take_out/src/main/java/com/itheima/controller/EmployeeController.java
+++ reggie_take_out/src/main/java/com/itheima/controller/EmployeeController.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.security.krb5.internal.crypto.DesCbcMd5EType;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 /**
@@ -28,9 +30,16 @@ import java.util.Map;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+/*
+1.拿到前端传过来的用户名与密码,进行加密;
+2.调用Service查询判断用户名是否存在(如果用户名不存在的话就没有继续的必要了);
+3.判断用户名与密码是否正确
+4.判断状态是否为1
+ */
 
     /**
      * 登录功能
+     *
      * @param request
      * @param employee
      * @return
@@ -69,18 +78,33 @@ public class EmployeeController {
 
     /**
      * 登出功能
+     *
      * @param request
      * @return
      */
+    @PostMapping("/logout")
     public R<String> logout(HttpServletRequest request) {
         HttpSession session = request.getSession();
         session.removeAttribute("employee");
         return R.success("登出成功");
     }
+
+    /**
+     * 新增员工
+     * @param employee
+     * @param request
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Employee employee,HttpServletRequest request) {
+        log.info("新增员工,员工信息:{}",employee.toString());
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+        employeeService.save(employee);
+        return R.success("新增员工成功");
+    }
 }
-/*
-1.拿到前端传过来的用户名与密码,进行加密;
-2.调用Service查询判断用户名是否存在(如果用户名不存在的话就没有继续的必要了);
-3.判断用户名与密码是否正确
-4.判断状态是否为1
- */
