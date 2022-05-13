55de8bb438a3730f58bf87034594b1b5c4525a6d
Parker74 <1227298453@qq.com>
2022-05-13 08:38:00 +0800

    新增员工

--- reggie_take_out/src/main/java/com/itheima/controller/EmployeeController.java
+++ reggie_take_out/src/main/java/com/itheima/controller/EmployeeController.java
@@ -11,13 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.security.krb5.internal.crypto.DesCbcMd5EType;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
-import java.util.Map;
 
 /**
  * @author HaoXiaoLong
@@ -30,12 +27,6 @@ import java.util.Map;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
-/*
-1.拿到前端传过来的用户名与密码,进行加密;
-2.调用Service查询判断用户名是否存在(如果用户名不存在的话就没有继续的必要了);
-3.判断用户名与密码是否正确
-4.判断状态是否为1
- */
 
     /**
      * 登录功能
@@ -91,13 +82,14 @@ public class EmployeeController {
 
     /**
      * 新增员工
+     *
      * @param employee
      * @param request
      * @return
      */
     @PostMapping
-    public R<String> save(@RequestBody Employee employee,HttpServletRequest request) {
-        log.info("新增员工,员工信息:{}",employee.toString());
+    public R<String> save(@RequestBody Employee employee, HttpServletRequest request) {
+        log.info("新增员工,员工信息:{}", employee.toString());
         employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
